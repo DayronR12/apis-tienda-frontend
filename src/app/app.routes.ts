fb1f7d1ea0d8a013bc459dd5106b2d527c36b31c
Dayron <dayronrestrepo0212@gmail.com>
2025-11-07 21:19:20 -0500

    ADD: Se agrega componente principal
    Configuración de rutas (Ajustes)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { PaginaPrincipalComponent } from './componentes/pagina-principal/pagina-principal.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: PaginaPrincipalComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+];
---
+++ src/app/componentes/pagina-principal/pagina-principal.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pagina-principal',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './pagina-principal.component.html',
+  styleUrl: './pagina-principal.component.css'
+})
+export class PaginaPrincipalComponent {
+
+}
